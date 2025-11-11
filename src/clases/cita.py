804359877bcb4491122139f25d5c5babe5f1ac51
DiegoUFV <150007304+DiegoUFV@users.noreply.github.com>
2025-11-11 20:21:23 +0100

    Add classes for managing appointments

--- src/clases/cita.py
+++ src/clases/cita.py
@@ -1,1 +1,20 @@
+class RegistrarCita:
+    pass
 
+class CambiarEstadoCita:
+    pass
+
+class EditarCita:
+    pass
+
+class EliminarCita:
+    pass
+
+class ListarCitasPorFecha:
+    pass
+
+class ListarCitasPorMascota:
+    pass
+
+class ListarCitasDelDia:
+    pass
