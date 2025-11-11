0d53ea12b3ff8b614b3d1ccb53f87c563e2f8f0d
DiegoUFV <150007304+DiegoUFV@users.noreply.github.com>
2025-11-11 20:23:59 +0100

    Update factura.py

--- src/clases/factura.py
+++ src/clases/factura.py
@@ -1,1 +1,17 @@
+class CrearFactura:
+    pass
 
+class AñadirLineaFactura:
+    pass
+
+class CalculoAutomaticoFactura:
+    pass
+
+class CambiarEstadoFactura:
+    pass
+
+class VisualizarFacturas:
+    pass
+
+class ExportarFactura:
+    pass
