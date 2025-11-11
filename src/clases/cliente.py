9fa6ac56e0181a93f6f3b161d1bd728e000347d8
DiegoUFV <150007304+DiegoUFV@users.noreply.github.com>
2025-11-11 20:20:48 +0100

    Add client management classes in cliente.py

--- src/clases/cliente.py
+++ src/clases/cliente.py
@@ -1,1 +1,18 @@
 
+class AltaCliente:
+    pass
+
+class BajaCliente:
+    pass
+
+class ModificarCliente:
+    pass
+
+class BuscarCliente:
+    pass
+
+class ListadoClientes:
+    pass
+
+class DetalleCliente:
+    pass
