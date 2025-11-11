51182548d9bed7169a855d28cfcbf16a536cc0ed
DiegoUFV <150007304+DiegoUFV@users.noreply.github.com>
2025-11-11 20:23:47 +0100

    Update historial.py

--- src/clases/historial.py
+++ src/clases/historial.py
@@ -1,1 +1,14 @@
+class AñadirNotaConsulta:
+    pass
 
+class RegistrarTratamiento:
+    pass
+
+class RegistrarVacunacion:
+    pass
+
+class HistorialClinicoCompleto:
+    pass
+
+class AdjuntarArchivos:
+    pass
